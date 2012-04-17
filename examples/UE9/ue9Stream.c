1a2f22665fa78b1a80900e71a9a034f873749618
LabJack <support@labjack.com>
2012-04-17 16:53:11 -0600

    Updated the UE9 stream example and fixed a seg. fault bug when the NUM_CHANNELS constant was not 4.

--- examples/UE9/ue9Stream.c
+++ examples/UE9/ue9Stream.c
@@ -1,9 +1,10 @@
 //Author: LabJack
-//May 25, 2011
+//April 17, 2012
 //This example program reads analog inputs AI0-AI3 using stream mode.
 
 #include "ue9.h"
-
+#include <math.h>
+#include <stdio.h>
 
 int StreamConfig_example(HANDLE hDevice);
 int StreamStart(HANDLE hDevice);
@@ -12,9 +13,9 @@ int StreamStop(HANDLE hDevice);
 int flushStream(HANDLE hDevice);
 int doFlush(HANDLE hDevice);
 
-const int ainResolution = 12;
-const uint8 NumChannels = 4;  //For this example to work proper, NumChannels
-                              //needs to be 1, 2, 4, 8 or 16
+const int AIN_RESOLUTION = 12;
+const uint8 NUM_CHANNELS = 4;  //Set this to a value between 1-16.  Readings
+                               //are performed on AIN channels 0-(NUM_CHANNELS-1).
 
 int main(int argc, char **argv)
 {
@@ -51,16 +52,16 @@ done:
 int StreamConfig_example(HANDLE hDevice)
 {
     int sendBuffSize;
-    sendBuffSize = 12 + 2*NumChannels;
+    sendBuffSize = 12 + 2*NUM_CHANNELS;
     uint8 sendBuff[sendBuffSize], recBuff[8];
     uint16 checksumTotal, scanInterval;
     int sendChars, recChars, i;
 
-    sendBuff[1] = (uint8)(0xF8);    //Command byte
-    sendBuff[2] = NumChannels + 3;  //Number of data words : NumChannels + 3
-    sendBuff[3] = (uint8)(0x11);    //Extended command number
-    sendBuff[6] = (uint8)NumChannels;  //NumChannels
-    sendBuff[7] = ainResolution;  //Resolution
+    sendBuff[1] = (uint8)(0xF8);  //Command byte
+    sendBuff[2] = NUM_CHANNELS + 3;  //Number of data words : NUM_CHANNELS + 3
+    sendBuff[3] = (uint8)(0x11);  //Extended command number
+    sendBuff[6] = (uint8)NUM_CHANNELS;  //NumChannels
+    sendBuff[7] = AIN_RESOLUTION;  //Resolution
     sendBuff[8] = 0;  //SettlingTime = 0
     sendBuff[9] = 0;  //ScanConfig: scan pulse and external scan
                       //trigger disabled stream clock
@@ -68,12 +69,12 @@ int StreamConfig_example(HANDLE hDevice)
 
     scanInterval = 4000;
     sendBuff[10] = (uint8)(scanInterval&0x00FF);  //Scan interval (low byte)
-    sendBuff[11] = (uint8)(scanInterval/256);     //Scan interval (high byte)
+    sendBuff[11] = (uint8)(scanInterval/256);  //Scan interval (high byte)
 
-    for( i = 0; i < NumChannels; i++ )
+    for( i = 0; i < NUM_CHANNELS; i++ )
     {
-        sendBuff[12 + i*2] = i; //channel # = i
-        sendBuff[13 + i*2] = 0; //BipGain (Bip = unipolar, Gain = 1)
+        sendBuff[12 + i*2] = i;  //channel # = i
+        sendBuff[13 + i*2] = 0;  //BipGain (Bip = unipolar, Gain = 1)
     }
 
     extendedChecksum(sendBuff, sendBuffSize);
@@ -186,17 +187,20 @@ int StreamStart(HANDLE hDevice)
     return 0;
 }
 
-//Reads the StreamData low-level function response in a loop.  All voltages from
-//the stream are stored in the voltages 2D array.
+//Reads the StreamData low-level function response in a loop.  All voltages
+//from the stream are stored in the voltages 2D array.
 int StreamData_example(HANDLE hDevice, ue9CalibrationInfo *caliInfo)
 {
     uint16 voltageBytes, checksumTotal;
     int recChars, backLog, overflow;
     int i, j, k, m, packetCounter, currChannel, scanNumber;
-    int totalPackets;        //The total number of StreamData responses read
-    int numDisplay;          //Number of times to display streaming information
-    int numReadsPerDisplay;  //Number of packets to read before displaying streaming information
+    int totalPackets;  //The total number of StreamData responses read
+    int numDisplay;  //Number of times to display streaming information
+    int numReadsPerDisplay;  //Number of packets to read before displaying
+                             //streaming information
     int readSizeMultiplier;  //Multiplier for the StreamData receive buffer size
+    int totalScans;  //Total scans that will be read  Meant for calculating the
+                     //size of the voltages array.
     long startTime, endTime;
 
     packetCounter = 0;
@@ -208,11 +212,12 @@ int StreamData_example(HANDLE hDevice, ue9CalibrationInfo *caliInfo)
     numReadsPerDisplay = 3;
     readSizeMultiplier = 10;
 
-    /* Each StreamData response contains (16/NumChannels) * readSizeMultiplier
+    /* Each StreamData response contains (16/NUM_CHANNELS) * readSizeMultiplier
      * samples for each channel.
-     * Total number of scans = (16 / NumChannels) * 4 * readSizeMultiplier * numReadsPerDisplay * numDisplay
+     * Total number of scans = (16 / NUM_CHANNELS) * 4 * readSizeMultiplier * numReadsPerDisplay * numDisplay
      */
-    double voltages[(16/NumChannels)*4*readSizeMultiplier*numReadsPerDisplay*numDisplay][NumChannels];
+    totalScans = ceil((16.0/NUM_CHANNELS)*4.0*readSizeMultiplier*numReadsPerDisplay*numDisplay);
+    double voltages[totalScans][NUM_CHANNELS];
     uint8 recBuff[192*readSizeMultiplier];
 
     printf("Reading Samples...\n");
@@ -223,12 +228,12 @@ int StreamData_example(HANDLE hDevice, ue9CalibrationInfo *caliInfo)
     {
         for( j = 0; j < numReadsPerDisplay; j++ )
         {
-            /* For USB StreamData, use Endpoint 2 for reads and 192 byte packets
-             * instead of the 46.  The 192 byte response is 4 StreamData packet
-             * responses of 48 bytes.
-             * You can read the multiple StreamData responses of 192 bytes to help
-             * improve streaming performance.  In this example this multiple is
-             * adjusted by the readSizeMultiplier variable.
+            /* For USB StreamData, use Endpoint 2 for reads and 192 byte
+             * packets instead of the 46.  The 192 byte response is 4
+             * StreamData packet responses of 48 bytes.
+             * You can read the multiple StreamData responses of 192 bytes to
+             * help improve streaming performance.  In this example this
+             * multiple is adjusted by the readSizeMultiplier variable.
              */
 
             //Reading response from UE9
@@ -299,9 +304,9 @@ int StreamData_example(HANDLE hDevice, ue9CalibrationInfo *caliInfo)
                 for( k = 12; k < 43; k += 2 )
                 {
                     voltageBytes = (uint16)recBuff[m*48 + k] + (uint16)recBuff[m*48 + k+1]*256;
-                    getAinVoltCalibrated(caliInfo, (uint8)(0x00), ainResolution, voltageBytes, &(voltages[scanNumber][currChannel]));
+                    getAinVoltCalibrated(caliInfo, (uint8)(0x00), AIN_RESOLUTION, voltageBytes, &(voltages[scanNumber][currChannel]));
                     currChannel++;
-                    if( currChannel > 3 )
+                    if( currChannel >= NUM_CHANNELS )
                     {
                         currChannel = 0;
                         scanNumber++;
@@ -340,12 +345,12 @@ int StreamData_example(HANDLE hDevice, ue9CalibrationInfo *caliInfo)
         printf("Current PacketCounter: %d\n", ((packetCounter == 0) ? 255 : packetCounter-1));
         printf("Current Comm backlog: %d\n", backLog);
 
-        for( k = 0; k < 4; k++ )
-            printf("  AI%d: %.4f V\n", k, voltages[scanNumber - 1][k]);
+        for( k = 0; k < NUM_CHANNELS; k++ )
+            printf("  AIN%d: %.4f V\n", k, voltages[scanNumber - 1][k]);
     }
 
     endTime = getTickCount();
-    printf("\nRate of samples: %.0lf samples per second\n", (scanNumber*NumChannels)/((endTime - startTime)/1000.0));
+    printf("\nRate of samples: %.0lf samples per second\n", (scanNumber*NUM_CHANNELS)/((endTime - startTime)/1000.0));
     printf("Rate of scans: %.0lf scans per second\n\n", scanNumber/((endTime - startTime)/1000.0));
 
     return 0;
@@ -421,7 +426,7 @@ int flushStream(HANDLE hDevice)
         else
             printf("Error : did not write all of the buffer (flushStream).\n");
         return -1;
-    } 
+    }
 
     //Reading response from UE9
     recChars = LJUSB_Read(hDevice, recBuff, 2);
@@ -443,18 +448,19 @@ int flushStream(HANDLE hDevice)
     return 0;
 }
 
-//Runs StreamStop and flushStream low-level functions to flush out the streaming buffer.  Also
-//reads 128 bytes from streaming endpoint to clear any left over data.  If there is nothing to
-//read from the streaming endpoint, then there will be a timeout delay.  This function is
-//useful for stopping streaming and clearing it after a Comm buffer overflow.
+//Runs StreamStop and flushStream low-level functions to flush out the
+//streaming buffer.  Also reads 128 bytes from streaming endpoint to clear any
+//left over data.  If there is nothing to read from the streaming endpoint,
+//then there will be a timeout delay.  This function is useful for stopping
+//streaming and clearing it after a Comm buffer overflow.
 int doFlush(HANDLE hDevice)
 {
-    uint8 recBuff[128];
+    uint8 recBuff[192];
 
     printf("Flushing stream and reading left over data.\n");
     StreamStop(hDevice);
     flushStream(hDevice);
-    LJUSB_Stream(hDevice, recBuff, 128);
+    LJUSB_Stream(hDevice, recBuff, 192);
 
     return 0;
 }
